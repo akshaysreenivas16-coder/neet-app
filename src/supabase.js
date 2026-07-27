ae185f4c22025eb123351123b1fa93968405a67e
Akshay Sreenivas <akshaysreenivas16@gmail.com>
2026-07-27 13:17:25 +0530

    fix auth in supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,15 @@
-import {createClient} from '@supabase/supabase-js'
-    const supabase= createClient(
-       import.meta.env.VITE_SUPABASE_URL,
-       import.meta.env.VITE_SUPABASE_KEY
-    )
- export default supabase;   
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_KEY,
+    {
+        auth: {
+            detectSessionInUrl: true,
+            persistSession: true,
+            autoRefreshToken: true
+        }
+    }
+)
+
+export default supabase
